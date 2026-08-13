dd2a863a9b29b92fb0195bf9e778d97df444b977
SahilShahCse <sahilshahcse@users.noreply.github.com>
2026-08-13 17:05:12 +0530

    the dashboard, made properly

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3268920896" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2648267230" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
